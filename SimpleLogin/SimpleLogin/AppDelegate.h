22b6b6867706f2100f6158e65b352fb1627e1669
SSASoft <nvd.mcs@gmail.com>
2018-01-21 21:06:28 +0500

    Inital Project

---
+++ SimpleLogin/SimpleLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleLogin
+//
+//  Created by SSASoft on 1/21/18.
+//  Copyright © 2018 SSASoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
